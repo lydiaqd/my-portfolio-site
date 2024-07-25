4fe43136bf86eb007a421544bc401bda0f23a859
lydiaqd <75919027+lydiaqd@users.noreply.github.com>
2024-07-25 01:04:28 -0500

    Added about me page

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -13,15 +13,15 @@ const HeroSection = () => {
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
             </div>
-            <div className="col-span-5">
-                <div className="rounded-full w-500 w-500">
-                    <Image src="/images/girl.jpg"
-                    alt="Girl Image"
-                    width={300}
-                    height={300}
-                    />
-                </div>
-            </div>
+            <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/girl.jpg"
+              alt="hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
         </div>
     </section>
   );
