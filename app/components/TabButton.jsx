3d527c8c20f7e1b420115a08867da6405a7dafe8
lydiaqd <75919027+lydiaqd@users.noreply.github.com>
2024-08-11 19:36:24 -0500

    TabButton component

--- app/components/TabButton.jsx
+++ app/components/TabButton.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const TabButton = () => {
+const TabButton = ({active, selectTab, children}) => {
+const buttonClasses = active ? 'text-white border-b border-purple-500' : 'text-[#ADB7BE]'
   return (
     <button className="mr-3 font-semibold hover:text-white text-[#ADB7BE] border-b border-purple-500">
-                    Skills
+        {children}
     </button>
   )
 }
