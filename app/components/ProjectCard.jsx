355b4faac1e1bf3a9d7ecb865bae18b5311c8aa2
lydiaqd <75919027+lydiaqd@users.noreply.github.com>
2024-09-13 23:05:52 -0500

    added images to cards

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -5,8 +5,7 @@ import Link from "next/link"
 const ProjectCard = (imgUrl, title, description ) => {
   return (
     <div>
-        <div className="h-2, md:h-72 rounded-t-xl relative group"
-    style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}>
+        <div className="h-2, md:h-72 rounded-t-xl relative group" style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}>
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
           <Link href="/" className="h-14 w-14 relative rounded-full border-[#ADB7BE] hover:border-white group/link">
             <CodeBracketIcon className="h-10 w-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[#ADB7BE] cursor-pointer hover:white" />
