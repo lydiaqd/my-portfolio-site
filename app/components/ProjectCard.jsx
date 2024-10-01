15c53c54d80880e7fd74f60a09ee4d1940d720e3
lydiaqd <75919027+lydiaqd@users.noreply.github.com>
2024-10-01 03:03:41 -0500

    transparency fix

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -3,10 +3,11 @@ import React from "react";
 const ProjectCard = ({ imgUrl, title, description }) => {
   return (
     <div>
-      <div className="h52 md:h-72" style={{background: `url(${imgUrl})`, backgroundSize: "cover"}}></div>
-      <div className="text-white">
-      <h5>{title}</h5>
-      <p>{description}</p>
+      <div className="h52 md:h-72 rounded-t-xl relative group" 
+      style={{background: `url(${imgUrl})`, backgroundSize: "cover"}}><div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 "></div></div>
+      <div className="text-white rounded-b-xl bg-[#181818] py-6 px-4">
+      <h5 className="text-xl font-semibold mb-2">{title}</h5>
+      <p className="text[#ADB7BE]">{description}</p>
       </div>
     </div>
   )
