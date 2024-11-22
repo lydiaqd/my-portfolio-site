0e06c881c0665eadd73f9978a40dc00468746af7
lydiaqd <75919027+lydiaqd@users.noreply.github.com>
2024-11-21 21:19:47 -0600

    Finished email API

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -5,11 +5,15 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
+/*export async function POST() {
+  return Response.json({ message: 'Hello World' })
+} */
+
 export async function POST() {
   try {
     const { data, error } = await resend.emails.send({
-      from: 'Lydia <lydiaqdames@gmail.com>',
-      to: ['lydiaqdames@gmail.com'],
+      from: 'lydiaqdames@gmail.com',
+      to: 'lydiaqdames@gmail.com',
       subject: 'Hello world',
       react: <>
                 <p>Email Body</p>
